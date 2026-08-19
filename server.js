a500bdd9ea42cf230b72a9f045f65d92a40a622c
jeenaanandita <jeena.anandita@dtgrowthteams.com>
2026-08-19 16:01:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/hello' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
